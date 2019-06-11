25a31b07cdd3a3aade5d5ff97a48e9a8a5138d9f
Aleksey Simuskov <opiumofthepeople@yandex.ru>
2019-06-11 23:48:14 +0300

    Added kinopoisk request interface with implementation

---
+++ clients/kinopoisk_api/client.go
@@ -0,0 +1,80 @@
+package kinopoisk
+
+import (
+	"crypto/md5"
+	"crypto/rand"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/an-death/go-kino/clients"
+)
+
+const (
+	KINOPOISK_API_SALT = "IDATevHDS7"
+)
+
+type client struct {
+	clients.Doer
+	baseUlr        string
+	clientId, uuid string
+}
+
+func NewAPIClient(do clients.Doer) clients.APIClient {
+	var clientId = make([]byte, 16, 16)
+	var uuid = make([]byte, 12, 12)
+	rand.Read(clientId)
+	rand.Read(uuid)
+	return &client{
+		Doer:     do,
+		clientId: fmt.Sprintf("%x", clientId),
+		uuid:     fmt.Sprintf("%x", uuid),
+	}
+}
+
+func (c *client) Request(method, baseUrl, uri string) (*http.Response, error) {
+	var err error
+	uri, err = c.uriWithUUID(uri)
+	if err != nil {
+		return nil, err
+	}
+
+	timeNow := time.Now()
+	timestamp := strconv.FormatInt(timeNow.UnixNano(), 10)
+	signature := c.createSignature(uri + timestamp + KINOPOISK_API_SALT)
+	request, err := http.NewRequest(method, baseUrl+uri, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "Android client (6.0.1 / api23), ru.kinopoisk/4.6.5 (86)")
+	request.Header.Add("Image-Scale", "3")
+	request.Header.Add("device", "android")
+	request.Header.Add("ClientId", c.clientId)
+	request.Header.Add("countryID", "2")
+	request.Header.Add("cityID", "1")
+	request.Header.Add("Android-Api-Version", "23")
+	request.Header.Add("clientDate", timeNow.Format("03:04 02.01.2006"))
+	request.Header.Add("X-TIMESTAMP", timestamp)
+	request.Header.Add("X-SIGNATURE", signature)
+
+	return c.Doer.Do(request)
+
+}
+
+func (c *client) uriWithUUID(baseURI string) (string, error) {
+	uri, err := url.Parse(baseURI)
+	if err != nil {
+		return "", nil
+	}
+	q := uri.Query()
+	q.Add("uuid", c.uuid)
+	uri.RawQuery = q.Encode()
+	return uri.String(), nil
+}
+
+func (c *client) createSignature(from string) string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(from)))
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/an-death/go-kino
 
 go 1.12
 
-require github.com/stretchr/testify v1.3.0
+require (
+	github.com/bouk/monkey v1.0.1
+	github.com/hexdigest/gounit v0.0.0-20180817093830-f1874d3307cb // indirect
+	github.com/shibukawa/configdir v0.0.0-20170330084843-e180dbdc8da0 // indirect
+	github.com/stretchr/testify v1.3.0
+	golang.org/x/tools v0.0.0-20190611201305-d303ba255abc // indirect
+)
