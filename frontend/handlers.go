c78e12ac1dc2f2400c82a2f1a9841551b7b0062c
Aleksey Simuskov <opiumofthepeople@yandex.ru>
2020-11-22 19:46:47 +0300

    Replaces releases endpoint and handlers with seach

--- frontend/handlers.go
+++ frontend/handlers.go
@@ -3,81 +3,32 @@ package frontend
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
-	"time"
 
-	"github.com/an-death/go-kino/providers/releases"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/gin-gonic/gin"
 )
 
-var releaseProvider releases.ReleaseProvider
-
-func init() {
-	releaseProvider = releases.NewKinopoiskProvider()
-}
-
-// func indexHandler(c *gin.Context) {
-// 	now := time.Now()
-// 	query := url.Values{
-// 		"from": []string{now.AddDate(0, -1, 0).Format("2006-01")},
-// 		"to":   []string{now.Format("2006-01")},
-// 	}
-// 	newUrl := url.URL{
-// 		Path:     "releases",
-// 		RawQuery: query.Encode(),
-// 	}
-// 	c.Redirect(http.StatusMovedPermanently, newUrl.RequestURI())
-// }
-
-func handleHTTP(w http.ResponseWriter, req *http.Request) {
-	resp, err := http.DefaultTransport.RoundTrip(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusServiceUnavailable)
-		return
-	}
-	defer resp.Body.Close()
-	copyHeader(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body)
-}
-func copyHeader(dst, src http.Header) {
-	for k, vv := range src {
-		for _, v := range vv {
-			dst.Add(k, v)
-		}
-	}
-}
-
-type requestTimeRange struct {
-	From time.Time `form:"from" time_format:"2006-01" binding:"required"`
-	To   time.Time `form:"to" time_format:"2006-01" binding:"required"`
-}
-
-func releasesHandler(c *gin.Context) {
-	var rr requestTimeRange
-	if err := c.ShouldBindQuery(&rr); err != nil {
-		c.String(http.StatusBadRequest, err.Error())
-		return
-	}
-
-	newReleases := releaseProvider.GetReleases(rr.From, rr.To)
-	c.HTML(http.StatusOK, "index.html", newReleases)
-}
-
-func torrentFileProxy(c *gin.Context) {
-	torrentFileUrl := c.Query("url")
-	if torrentFileUrl == "" {
+func proxy(c *gin.Context) {
+	target := c.Query("url")
+	if target == "" {
 		c.Status(http.StatusServiceUnavailable)
 		return
 	}
 
-	response, err := http.Get(torrentFileUrl)
+	response, err := http.Get(target)
 	if err != nil || response.StatusCode != http.StatusOK {
 		c.Status(http.StatusServiceUnavailable)
 		return
 	}
 
+	proxyResponse(response, c)
+}
+
+func proxyResponse(response *http.Response, c *gin.Context) {
 	reader := response.Body
 	contentLength := response.ContentLength
 	contentType := response.Header.Get("Content-Type")
@@ -88,6 +39,7 @@ func torrentFileProxy(c *gin.Context) {
 	}
 
 	c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+
 }
 
 func searchGet(c *gin.Context) {
@@ -99,28 +51,63 @@ func searchGet(c *gin.Context) {
 	searchQuery := fmt.Sprintf(`http://rutor.info/search/%s/?search_method=0&search_in=0&category=0&s_ad=0`, searchFilmName)
 	response, err := http.Get(searchQuery)
 	if err != nil {
+		fmt.Println(err)
 		c.Status(http.StatusServiceUnavailable)
 		return
 	}
-	reader := response.Body
-	contentLength := response.ContentLength
-	contentType := response.Header.Get("Content-Type")
-
-	extraHeaders := map[string]string{}
-	for headerName, headerValue := range response.Header {
-		extraHeaders[headerName] = strings.Join(headerValue, "; ")
+	result, err := replaceUrls(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		c.Status(http.StatusServiceUnavailable)
+		return
 	}
 
-	c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+	oldBody, newBody := response.Body, result
+	defer oldBody.Close()
+	response.Body = newBody
+	proxyResponse(response, c)
+}
+
+func replaceUrls(source io.Reader) (io.ReadCloser, error) {
+	doc, err := goquery.NewDocumentFromReader(source)
+	if err != nil {
+		return nil, err
+	}
+
+	base, _ := url.Parse("http://rutor.info/")
+	doc.Find("#index").Find("a").Each(func(_ int, link *goquery.Selection) {
+		href, ok := link.Attr("href")
+		if ok {
+			u, err := url.Parse(href)
+			if err != nil {
+				panic(err)
+			}
+			if u.IsAbs() {
+				link.ReplaceWithHtml("/proxy?url=" + href)
+			} else {
+				local, _ := url.Parse("/proxy")
+				q := local.Query()
+				q.Set("url", base.ResolveReference(u).String())
+				local.RawQuery = q.Encode()
+
+				link.ReplaceWithHtml(local.String())
+			}
+		}
+	})
+	ctn, err := doc.Html()
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.NopCloser(strings.NewReader(ctn)), nil
 }
 
 func routes() *gin.Engine {
 	router := gin.Default()
 	router.Static("/static", "./frontend/static")
 	router.LoadHTMLFiles("frontend/html/index.html", "frontend/html/movie.html", "frontend/html/search.html")
-	router.GET("/", gin.WrapF(handleHTTP))
-	router.GET("/releases", releasesHandler)
-	router.GET("/proxy", torrentFileProxy)
+	router.GET("/", searchGet)
+	router.GET("/proxy", proxy)
 	router.GET("/search", searchGet)
 
 	return router
